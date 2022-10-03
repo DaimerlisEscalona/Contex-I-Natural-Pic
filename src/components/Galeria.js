6b3950753937d4e48dda3d600524d8a81c80ccf0
DaimerlisEscalona <daimerlisescalona@gmail.com>
2022-10-03 15:34:41 -0300

    modificacion del package.json

--- src/components/Galeria.js
+++ src/components/Galeria.js
@@ -7,21 +7,24 @@ export default function Home() {
 
   const { fotos, setFavoritos, favoritos } = useContext(Context);
 
-  const agregarFavoritos = (e) =>{
-    
-    if(fotos != favoritos){
+  const agregarFavoritos = (e) => {
+
+    if (e.liked == false) {
+      e.liked = true
       setFavoritos([...favoritos, e]);
-      
+    } else {
+      e.liked = false
+      const newFavoritos = favoritos.filter((favorito) => favorito.id !== e.id)
+      setFavoritos(newFavoritos)
     }
-      
-  };
+  }
 
   return (
     <div className="galeria grid-columns-5 p-3">
       {fotos.map((e) => (
         <div 
           key={e.id}
-          onClick={() => (e.liked = true, agregarFavoritos(e)) }
+          onClick={() => {agregarFavoritos(e)}}
           className="foto"
           style={{backgroundImage: `url(${e.src.tiny})`}}
         >
